258a50cb8073480738d4f774004dffa0397b08b0
Ishan Manandhar <ishan02016@gmail.com>
2021-01-26 18:02:42 +0545

    initialize the project folders for demo app

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import '../stylesheets/Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <h2>Airtable and React CRUD Application</h2>
+    </div>
+  );
+}
+
+export default Home;
